c575d4fcddd2eda7cb9999f18b0a8ec909d4209f
rhit-cerasoml <cerasoml@rose-hulman.edu>
2024-12-23 00:11:42 -0500

    add code to pop window + server/client packages

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import client.TankTrouble;
+
+public class Main {
+    public static void main(String[] args){
+        TankTrouble.main("client.TankTrouble");
+    }
+}
---
+++ src/client/TankTrouble.java
@@ -0,0 +1,23 @@
+package client;
+
+import processing.core.PApplet;
+import processing.core.PGraphics;
+
+public class TankTrouble extends PApplet {
+
+    @Override
+    public void start() {
+        super.start();
+    }
+
+    @Override
+    public void setup() {
+        super.setup();
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(500, 500);
+    }
+}
